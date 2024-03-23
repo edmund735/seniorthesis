02f938fc41ad5ea37bd35a2baf265f723a2c0967
Edmund <edmund666@princeton.edu>
2024-03-23 19:30:25 -0400

    env checker

--- check_env.py
+++ check_env.py
@@ -1,5 +1,12 @@
 # import gymnasium
-from gymnasium import gymnasium.utils.env_checker.check_env
+# from gymnasium.utils.env_checker import checkenv
 from trading_env import TradingEnv
+from stable_baselines3.common.env_checker import check_env
+from gymnasium.wrappers import RescaleAction
 
-check_env(TradingEnv)
+env = RescaleAction(TradingEnv(), min_action = -1., max_action = 1.)
+print(env.action_space)
+# It will check your custom environment and output additional warnings if needed
+check_env(env)
+
+# check_env(TradingEnv)
