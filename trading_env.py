8cc1cc85008d94e4a5f9a56c8a1d19395cea30de
Adroit <edmund666@princeton.edu>
2024-03-23 22:53:19 -0400

    Fixed trading env

--- trading_env.py
+++ trading_env.py
@@ -1,6 +1,6 @@
 import numpy as np
 import gymnasium as gym
-from gymnasium import spaces
+from gymnasium import spaces, logger
 
 
 class TradingEnv(gym.Env):
@@ -11,8 +11,8 @@ class TradingEnv(gym.Env):
                  render_mode=None,
                  T = 100.,
                  impact_decay = 10.,
-                 lamb = 0.001,
-                 gamma = 0.01,
+                 lamb = 0.01,
+                 gamma = 1,
                  sigma = 0.3,
                  c = 0.5,
                  target_q = 1000,
@@ -27,20 +27,23 @@ class TradingEnv(gym.Env):
         assert S0 > 0, "Initial price must be positive"
 
         self.T = T
+        self.T_rem = None
+        self.impact_decay = impact_decay
         self.lamb = lamb
         self.gamma = gamma
         self.sigma = sigma
         self.c = c
-        self.impact_decay = impact_decay
         self.target_q = target_q
-        self.S0 = S0 # asset price
+        self.S0 = S0
+        # self.bench_x = self.target_q / self.
 
         # for low and high, state variables are J, I, alpha, P, Q_rem, T_rem
-        low = np.array([0, 0, -np.inf, 0, 0, 0])
-        high = np.array([target_q, lamb * target_q ** c, np.inf, np.inf, target_q, T])
+        low = np.array([0, 0, -np.inf, -np.inf, 0, 0])
+        high = np.array([target_q, lamb * np.sign(target_q) * abs(target_q) ** c, np.inf, np.inf, target_q, T])
         self.observation_space = spaces.Box(low = low, high = high, dtype = np.float64)
 
-        self.action_space = spaces.Box(low = 0, high = target_q, shape = (1,), dtype = np.float64)
+        # Actions will be rescaled in step function
+        self.action_space = spaces.Box(low = -1., high = 1., shape = (1,), dtype = np.float32)
 
     def _get_obs(self):
         if self.c == 0.5:
@@ -48,7 +51,7 @@ class TradingEnv(gym.Env):
         else:
             I = self.lamb * np.sign(self.J) * abs(self.J) ** self.c
         P = self.S + I
-        return np.array([self.J, I, self.alpha, P, self.target_q - self.Q, self.T_rem], dtype = np.float64)
+        return np.array([self.J, I, self.alpha, P, self.Q_rem, self.T_rem], dtype = np.float64)
 
     def _get_info(self):
         return {}
@@ -61,7 +64,13 @@ class TradingEnv(gym.Env):
         self.J = 0. # EWMA of past and current order flow
         self.alpha = 0. # forecast
         self.S = self.S0 # asset price
-        self.Q = 0. # position
+        self.Q_rem = self.target_q # position
+
+        # Benchmark
+        # self.bench_J = 0.
+        # self.bench_alpa = 0.
+        # self.bench_S = self.S0
+        # self.Q = 0.
 
         observation = self._get_obs()
         info = self._get_info()
@@ -71,26 +80,39 @@ class TradingEnv(gym.Env):
 
     def step(self, action):
 
+        assert self.T_rem is not None, "You must call reset() before step()"
+        if self.T_rem <= 0:
+            logger.warn("This episode has already terminated. Please call reset() before step()")
+        if not self.action_space.contains(action):
+            if self.action_space.contains(action.astype(np.float32)):
+                logger.warn(f"Actions should be of type np.float32 but are currently {type(action[0])}")
+            else:
+                assert self.action_space.contains(action), f"The action {action!r} ({type(action)}) is invalid (actions must be in [-1,1])"
+
+        # Passed in action is in [-1,1], need to rescale to [0,Q_rem]
+        x = (action[0] + 1)/2*self.Q_rem
+
+        # Must reach target_q by last time step
+        if self.T_rem <= 1:
+            x = self.Q_rem
+
         # Dynamics of price impact
-        action = action[0]
-        self.J = np.exp(-1/self.impact_decay) * self.J + action
+        self.J = np.exp(-1/self.impact_decay) * self.J + x
         self.alpha = np.exp(-1/self.impact_decay) * self.alpha + self.gamma * self.np_random.standard_normal()
         prev_S = self.S
         self.S += self.alpha + self.sigma * self.np_random.standard_normal()
-        self.Q += action
+        self.Q_rem -= x
         self.T_rem -= 1
 
-        # An episode is terminated if the current quantity is above the target or there's no more time
-        terminated = True if self.Q >= self.target_q or self.T_rem <= 0 else False
+        # Benchmark
+        # bench_action = self.target_q / self.T
 
         observation = self._get_obs()
-
-        # if bought too many shares
-        if self.Q - self.target_q >= 0.1:
-            reward = -np.inf
-        else:
-            reward = -action * observation[1] + self.Q * (self.S-prev_S)
-        
         info = self._get_info()
+        
+        reward = -x * observation[1] + (self.target_q - self.Q_rem - x) * (self.S-prev_S)
+
+        # An episode is terminated when there's no more time
+        terminated = True if self.T_rem <= 0 else False
 
         return observation, reward, terminated, False, info
