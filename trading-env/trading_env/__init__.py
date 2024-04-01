111162228008e7e0f66c1057cea6c3afbb18c54a
Edmund <edmund666@princeton.edu>
2024-04-01 16:27:34 -0400

    trading env

---
+++ trading-env/trading_env/__init__.py
@@ -0,0 +1,6 @@
+from gym.envs.registration import register
+
+register(
+    id='TradingEnv',
+    entry_point='trading_env.envs:TradingEnv',
+)
---
+++ trading-env/trading_env/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_examples.envs.trading_env import TradingEnv
